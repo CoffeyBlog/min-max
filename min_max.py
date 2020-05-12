06be7388996f6a6a345a68731f857ca20554cd8c
Coffey <lucascoffey@gmail.com>
2020-05-12 11:29:13 -0400

    Initial commit

---
+++ min_max.py
@@ -0,0 +1,19 @@
+
+
+ages = [45, 35, 25, 18, 21, 34, 23, 73, 63, 46, 73, 16]
+
+print(max(ages))
+
+print(min(ages))
+
+
+
+names = ["Aaron", "Zed"]
+
+# prints the name that is higher in alphabetical order
+print(max(names))
+
+
+# prints name that is lowest in alphabetical order
+print(min(names))
+
